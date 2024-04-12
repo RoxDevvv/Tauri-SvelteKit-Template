0274892f9ee89cae0e789fba3d48e8640a6391d4
Amine Laaraf <aminelaaraf@gmail.com>
2024-04-12 20:53:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Tauri Svelte template
+
+## Setup
+
+Clone the project and run this commands 
+
+```bash
+npm npm install
+```
+
+## Developing
+
+Once you've installed dependencies with `npm install` 
+
+```bash
+npm npm run tauri dev
+
+```
+
+## Building
+
+To create a production version of your app:
+
+```bash
+npm npm run tauri build
+```
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+	import { message } from '@tauri-apps/api/dialog';
+
+	function hello() {
+		  message('Svelte in Tauri', 'Message');
+	}
+</script>
+
+<div class="container">
+	<h1>SvelteKit ❤️️ Tauri</h1>
+	<h2>beautiful world</h2>
+    <button on:click={hello}>hello</button>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+	}
+	h1 {
+		color: white;
+	}
+	h2 {
+		color: white;
+	}
+    button {
+        background-color: rgb(255, 107, 107);
+        color: white;
+        border: none;
+        padding: 10px 20px;
+        border-radius: 15px;
+        font-size: 24px;
+        cursor: pointer;
+    }
+	:global(body) {
+		background: black;
+        overflow: hidden;
+	}
+</style>
